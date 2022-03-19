9c5bd2fa625fca47345b04cf082680588b8a3c2f
pontaryagin <ponta1496@gmail.com>
2022-03-19 23:33:35 +0900

    add is_shared

--- get_lock.py
+++ get_lock.py
@@ -11,8 +11,9 @@ import os
 from pathlib import Path
 
 @contextlib.contextmanager
-def file_lock_impl(file: str, stdin : IO[bytes], stdout : Union[IO[bytes],IO[str]]):
-    stdin.write(f"exec 9> '{file}'; flock -x 9; echo locked;\n".encode()) 
+def file_lock_impl(file: str, stdin : IO[bytes], stdout : Union[IO[bytes],IO[str]], is_shared):
+    opt = "-s" if is_shared else "-x"
+    stdin.write(f"exec 9> '{file}'; flock {opt} 9; echo locked;\n".encode()) 
     stdin.flush() # type: ignore
     line = stdout.readline() # type: ignore
     if isinstance(line, bytes):
@@ -22,38 +23,42 @@ def file_lock_impl(file: str, stdin : IO[bytes], stdout : Union[IO[bytes],IO[str
     yield
 
 @contextlib.contextmanager
-def file_lock(file: str):
-    p = subprocess.Popen(["/bin/bash"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, start_new_session=True,)
+def file_lock(file: str, is_shared = False):
+    p = subprocess.Popen(["/bin/bash"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, start_new_session=True)
     stdin : IO[bytes] = p.stdin # type: ignore
     stdout : IO[bytes] = p.stdout # type: ignore
     try:
-        with file_lock_impl(file, stdin, stdout):
+        with file_lock_impl(file, stdin, stdout, is_shared):
             yield
     finally:
         p.kill()        
 
 @contextlib.contextmanager
-def remote_file_lock(file: str, client: Optional[paramiko.SSHClient] = None, remote: Optional[str] = None):
+def remote_file_lock(file: str, client: Optional[paramiko.SSHClient] = None, remote: Optional[str] = None, is_shared = False):
     if client is None:
         assert(remote is not None)
         client = paramiko.SSHClient()
         client.load_host_keys(os.path.expanduser('~/.ssh/known_hosts'))
         client.connect(remote)
     stdin, stdout, _ = client.exec_command("/bin/bash")
-    with file_lock_impl(file, stdin, stdout):  # type: ignore
+    with file_lock_impl(file, stdin, stdout, is_shared):  # type: ignore
         yield
 
-def get_lock(file: str, is_shared: bool=False):
-    with file_lock(file):
+def get_lock(file: str, is_shared: bool):
+    with file_lock(file, is_shared):
         print(f"[get_lock] lock start {file}")
         time.sleep(2)
         print("[get_lock] lock end")
 
 def get_lock2(file: str, remote: str):
-    with remote_file_lock(file, remote="localhost"):
+    with remote_file_lock(file, remote=remote):
         print(f"[get_lock2] lock start {file=}")
         time.sleep(2)
         print("[get_lock2] lock end")
+
+for i in range(3):
+    p1=multiprocessing.Process(target=get_lock, args=("tmp", True))
+    p1.start()
 
 for i in range(3):
     time.sleep(1)
@@ -61,5 +66,6 @@ for i in range(3):
     p2=multiprocessing.Process(target=get_lock2, args=(Path("tmp").absolute(), "localhost"))
     p1.start()
     p2.start()
+
 
 
